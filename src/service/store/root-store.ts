a0e9c7b5733d71a628ceab9ddfe2f553c8e86fb0
strahsvid <maximsushkin02@gmail.com>
2024-03-13 20:58:19 +0300

    .

--- src/service/store/root-store.ts
+++ src/service/store/root-store.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { StoreState } from "./store-state.ts";
 
-export const rootStore = create<StoreState>()((set) => ({
+export const rootStore = create<StoreState>()(() => ({
   dialogStep: 1,
-  setStep: (set) => {},
+  setStep: () => {},
 }));
